0c6499ae26123d74a02e01f40779d0cba111bc4d
korrova11 <korrova@yandex.ru>
2024-02-10 04:21:40 +0300

    between 3 commit

--- src/main/resources/liquibase/scripts/indexpractice.sql
+++ src/main/resources/liquibase/scripts/indexpractice.sql
@@ -3,4 +3,4 @@
 -- changeset popova:1
 CREATE INDEX student_name_index ON student (name);
 -- changeset popova:2
-CREATE INDEX faculty_name-color_index ON users (name, color);
+CREATE INDEX faculty_name_color_index ON faculty (name, color);
